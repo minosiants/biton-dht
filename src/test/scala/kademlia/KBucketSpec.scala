a2f0f367867b90f5ca1b4def16de04f7ab6ec5a6
Kaspar Minosiants <kaspar.ru@gmail.com>
2020-05-07 19:46:15 +0300

    Add implicit Clock

--- src/test/scala/kademlia/KBucketSpec.scala
+++ src/test/scala/kademlia/KBucketSpec.scala
@@ -1,10 +1,29 @@
 package kademlia
+import java.time.{ Clock, Instant, ZoneId }
+
+import org.scalacheck._
+import cats.syntax.either._
 
 class KBucketSpec extends KSpec {
-  "KBucket" should {
-    "add to bucket" in {
 
-      success
+  "KBucket" should {
+
+    val ksize = 5
+
+    "add to full kbucket" in Prop.forAll(kbucketGen(0, ksize, 5), nodeGen) {
+      (kbucket, node) =>
+        val result = kbucket.add(node)
+        result ==== Error.KBucketError(s"$kbucket is full").asLeft
+    }
+
+    "add to empty kbucket" in Prop.forAll(kbucketGen(0, ksize, 0), nodeGen) {
+      (kbucket, node) =>
+        val result = kbucket.add(node)
+        result ==== KBucket.create(
+          kbucket.prefix,
+          Nodes(List(node), ksize),
+          kbucket.cache
+        )
     }
   }
 
