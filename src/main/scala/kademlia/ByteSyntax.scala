df27b76a1452c0540098c2f6ce98d6862c6d0a57
Kaspar Minosiants <kaspar.ru@gmail.com>
2020-05-07 22:57:39 +0300

    Add KBucket split test

---
+++ src/main/scala/kademlia/ByteSyntax.scala
@@ -0,0 +1,9 @@
+package kademlia
+
+trait ByteSyntax {
+  implicit def byteSyntax(b: Byte): ByteOps = new ByteOps(b)
+}
+
+final class ByteOps(val b: Byte) extends AnyVal {
+  def ubyte: Int = b & 0xFF
+}
--- src/test/scala/kademlia/KSpec.scala
+++ src/test/scala/kademlia/KSpec.scala
@@ -1,46 +1,56 @@
 package kademlia
 
-import java.time.{ Clock, Instant, LocalDateTime, ZoneId, ZoneOffset }
+import java.time.{Clock, Instant, LocalDateTime, ZoneOffset}
 
 import cats.effect.specs2.CatsIO
 import com.comcast.ip4s.IpAddress
 import kademlia.KBucket.Cache
+import kademlia.types._
 import org.scalacheck.Gen
 import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
-import types._
+import scodec.bits.BitVector
 
 class KSpec extends Specification with ScalaCheck with CatsIO {
 
   implicit val clock: Clock = Clock.fixed(Instant.now(), ZoneOffset.UTC)
 
-  val nodeIdIntGen: Gen[NodeId] = Gen.posNum[Int].map(NodeId.fromInt)
+  val nodeIdIntGen: Gen[NodeId] =
+    Gen.chooseNum(0, Integer.MAX_VALUE).map(NodeId.fromInt)
 
-  val ipV4Gen: Gen[IpAddress] = for {
-    one   <- Gen.posNum[Byte]
-    two   <- Gen.posNum[Byte]
-    three <- Gen.posNum[Byte]
-    four  <- Gen.posNum[Byte]
-  } yield IpAddress.fromBytes(Array(one, two, three, four)).get
+  val nodeIdCharGen: Gen[NodeId] =
+    Gen
+      .infiniteStream(Gen.alphaChar)
+      .map(_.take(20).toList)
+      .map(v => NodeId(BitVector(v.map(_.toByte))))
+
+  val byteGen: Gen[Byte] = Gen.chooseNum(0, 255).map(_.byteValue())
+
+  val ipV4Gen: Gen[IpAddress] =
+    Gen
+      .infiniteStream(byteGen)
+      .map(_.take(4).toArray)
+      .map(IpAddress.fromBytes(_).get)
 
   val nodeGen: Gen[Node] = for {
-    id   <- nodeIdIntGen
     ip   <- ipV4Gen
+    id   <- nodeIdCharGen
     port <- Gen.posNum[Int]
-  } yield Node(id, ip, port, LocalDateTime.now)
+  } yield Node(id, ip, port, LocalDateTime.now(clock))
 
   def listOfNodesGen(size: Int): Gen[List[Node]] =
     Gen
       .infiniteStream(nodeGen)
-      .map(_.take(size * 4).toSet.take(size).toList)
+      .map(_.take(size * 40).toSet.take(size).toList)
       .retryUntil(_.size == size)
 
   def kbucketGen(prefix: Int, ksize: Int, nsize: Int): Gen[KBucket] =
     for {
       nodes <- listOfNodesGen(nsize).map(v => Nodes(v, ksize))
+      cache <- listOfNodesGen(nsize).map(v => Cache(Nodes(v, ksize)))
       pref = Prefix(NodeId.fromInt(prefix).value)
     } yield KBucket
-      .create(pref, nodes, Cache(Nodes(List.empty, ksize * 4)))
+      .create(pref, nodes, cache)
       .toOption
       .get
 }
