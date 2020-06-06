919370adefebbead184be8101fd7962bef8b6d56
Kaspar Minosiants <kaspar.ru@gmail.com>
2020-06-06 22:40:18 +0300

    Add test for Secrets

---
+++ src/test/scala/biton/dht/SecretsSpec.scala
@@ -0,0 +1,32 @@
+package biton.dht
+import scala.concurrent.duration._
+import cats.syntax.eq._
+
+class SecretsSpec extends KSuite {
+
+  test("get the same secret") {
+    Secrets.create(1.second).use { secrets =>
+      for {
+        s1 <- secrets.get
+        s2 <- secrets.get
+      } yield s1 === s2
+    }
+  }
+  test("get different secret") {
+    Secrets.create(100.millis).use { secrets =>
+      for {
+        s1 <- secrets.get
+        _  <- ioTimer.sleep(110.millis)
+        s2 <- secrets.get
+      } yield s1 =!= s2
+    }
+  }
+  test("is valid secret") {
+    Secrets.create(100.millis).use { secrets =>
+      for {
+        s1      <- secrets.get
+        isValid <- secrets.isValid(s1)
+      } yield isValid
+    }
+  }
+}
